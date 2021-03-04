3ccde5b269564187fbcccf1f9b1c8e17562f9100
Nathan McDonald <nmcdona7@email.cpcc.edu>
2021-03-04 12:03:45 -0500

    Revert "Revert "Update HelloWorld.java""
    This reverts commit db35b7955c213a612a00b3600d5b441c51f7853f.

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,34 @@
-public class HelloWorld
+import java.util.Scanner;
+
+public class Temperature
 {
    public static void main(String[] args)
    {
-   System.out.println("Hello World!");
-   System.out.println("Program modified, but able to revert to original version using GitHub Desktop");
-   }
-}
+      //Variable declaration
+      double temp = 0;
+      
+      //Instantiating a Scanner object to get user input
+      Scanner sc = new Scanner(System.in);
+
+      //Get temperature from user
+      System.out.print( "Enter a temperature:  ");
+      temp = sc.nextInt();
+
+      //Display message based on temperature
+      if (temp <= 32) 
+          System.out.println("It's freezing.");
+    
+      if (temp < 50) 
+          System.out.println("It's cold.");
+    
+      if (temp < 80) 
+         System.out.println("It's warm.");
+    
+      if (temp < 100); 
+         System.out.println("It's hot.");
+    
+      if (temp < 120);
+         System.out.println("It's extremely hot!");
+         
+    }//end main
+}//end class
